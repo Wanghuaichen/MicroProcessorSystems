e58c65b06ec37cca8cd0f8f60f4b1215243bf3f8
tim8765 <tianming.zhang@mail.mcgill.ca>
2016-10-20 22:27:48 -0400

    piezo with peak detect

--- Lab3_STM32F4Cube_Base_project/src/main.c
+++ Lab3_STM32F4Cube_Base_project/src/main.c
@@ -12,19 +12,29 @@ volatile int systick_flag;
 
 int main(void) {
 	
-	float piezo_val;
+	int counter =0;
+	float piezo_val = 0;
+	float max = 0;
 	
 	system_init();
 
 	while(1) {
-		HAL_Delay(100);
+		//HAL_Delay(100);
 		
-		piezo_val=piezo_adc_poll();
-		printf("piezo value is %f\n", piezo_val);
-
-		float out[3];
-		LIS3DSH_ReadACC(out);
-		printf("x:%f y:%f z:%f\n",out[0],out[1],out[2]);
+		//peak and hold thingy
+		piezo_val = piezo_adc_poll();
+		counter++;
+		if(piezo_val>max){
+			max = piezo_val;
+		}
+		if (counter==1000){
+			printf("piezo value is %f\n", max);
+			max=0;
+			counter=0;
+		}
+		//float out[3];
+		//LIS3DSH_ReadACC(out);
+		//printf("x:%f y:%f z:%f\n",out[0],out[1],out[2]);
 	}
 	return 0;
 }
