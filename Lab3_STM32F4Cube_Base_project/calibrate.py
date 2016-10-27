fc8bcccea5a9e0bb55f36ae0e483cad3d23f95a8
Alex Bhandari-Young <alexbhandari@gmail.com>
2016-10-27 03:13:10 -0400

    calibrate.py working

--- Lab3_STM32F4Cube_Base_project/calibrate.py
+++ Lab3_STM32F4Cube_Base_project/calibrate.py
@@ -2,28 +2,28 @@ from sklearn import linear_model
 import csv
 
 X = []
-y = [] 
+Y = [] 
 
-with open('accelcal.csv', 'rb') as csvfile:
+with open('accelcal.csv', 'r') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
         x_thing = []
         y_thing = []
-        print ', '.join(row)
-        x_thing.append(row[0])
-        y_thing.append(row[1])
-        x_thing.append(row[3])
-        y_thing.append(row[4])
-        x_thing.append(row[5])
-        y_thing.append(row[6])
+        print (', '.join(row))
+        x_thing.append(float(row[0]))
+        y_thing.append(float(row[1]))
+        x_thing.append(float(row[2]))
+        y_thing.append(float(row[3]))
+        x_thing.append(float(row[4]))
+        y_thing.append(float(row[5]))
         X.append(x_thing)
         Y.append(y_thing)
         
 
-#X = [[1,2,3], [4,5,6], [7,8,9]]
-#y = [[1,0,0], [0,1,0], [0,0,1]]
+X = [[1,2,3], [4,5,6], [7,8,9]]
+y = [[1,0,0], [0,1,0], [0,0,1]]
 model = linear_model.LinearRegression()
 
 model.fit(X, y)
-print model.coef_
-print model.intercept_
+print (model.coef_)
+print (model.intercept_)
