f282f67bccaed574c425b12e184b644a66a14c41
Alex Bhandari-Young <alexbhandari@gmail.com>
2016-10-29 14:59:55 -0400

    calibrate.py

--- Lab3_STM32F4Cube_Base_project/calibrate.py
+++ Lab3_STM32F4Cube_Base_project/calibrate.py
@@ -2,7 +2,7 @@ from sklearn import linear_model
 import csv
 
 X = []
-Y = [] 
+y = [] 
 
 with open('accelcal.csv', 'r') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
@@ -11,17 +11,14 @@ with open('accelcal.csv', 'r') as csvfile:
         y_thing = []
         print (', '.join(row))
         x_thing.append(float(row[0]))
-        y_thing.append(float(row[1]))
+        x_thing.append(float(row[1]))
         x_thing.append(float(row[2]))
         y_thing.append(float(row[3]))
-        x_thing.append(float(row[4]))
+        y_thing.append(float(row[4]))
         y_thing.append(float(row[5]))
         X.append(x_thing)
-        Y.append(y_thing)
+        y.append(y_thing)
         
-
-X = [[1,2,3], [4,5,6], [7,8,9]]
-y = [[1,0,0], [0,1,0], [0,0,1]]
 model = linear_model.LinearRegression()
 
 model.fit(X, y)
