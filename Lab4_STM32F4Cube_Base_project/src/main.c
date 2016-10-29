44f7ad692e4b2866988525e80bf9ab03fa1327f0
Alex Bhandari-Young <alexbhandari@gmail.com>
2016-10-29 15:03:42 -0400

    init lab 4

---
+++ Lab4_STM32F4Cube_Base_project/src/main.c
@@ -0,0 +1,37 @@
+////////////////////////////////////////////////////////////////////////////////
+//	File Name					: main.c
+//	Description				: program entry
+//	Author						: Harsh Aurora
+//	Date							: Oct 28, 2016
+////////////////////////////////////////////////////////////////////////////////
+	
+//		Includes		//
+#include <stm32f4xx_hal.h>
+#include <supporting_functions.h>
+#include <sysclk_config.h>
+#include <lis3dsh.h>
+#include <arm_math.h>
+#include <LED_thread.h>
+#include <cmsis_os.h>
+
+
+//Brief:	main program
+//				
+//Params:	None
+//Return:	None
+int main(void) {
+  //		MCU Configuration		//
+  //	Reset of all peripherals, Initializes the Flash interface and the Systick	//
+  HAL_Init();
+	
+  //	Configure the system clock	//
+  SystemClock_Config();
+
+	
+	start_LED_thread(NULL);
+	
+	osKernelStart();
+	osDelay(osWaitForever);
+
+	return 0;
+}
